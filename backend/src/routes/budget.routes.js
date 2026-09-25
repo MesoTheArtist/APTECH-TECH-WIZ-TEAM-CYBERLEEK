a3f8bec3b1d542d3cfd38835f368e651ecf3b70c
williams-olanrewaju <owolabilanre2006@gmail.com>
2026-09-25 21:10:38 +0100

    fix: fixed backend code, and adjusted .gitignore

---
+++ backend/src/routes/budget.routes.js
@@ -0,0 +1,7 @@
+// Example: src/routes/auth.routes.js
+const express = require('express');
+const router = express.Router();
+
+// ... routes defined here ...
+
+module.exports = router; // 👈 CRITICAL: Must be at the very bottom
